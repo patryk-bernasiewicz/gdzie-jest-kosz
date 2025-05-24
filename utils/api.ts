1e3a05bb7bbf17c7dcfe085479a1d775966579a5
Patryk Bernasiewicz <patryk.bernasiewicz@gmail.com>
2025-05-24 21:52:26 +0200

    DEV-8: update backend API urls to use v1 prefix

--- utils/api.ts
+++ utils/api.ts
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 import { fetchAndSetClerkToken } from '@/feature/auth/store/clerkToken.util';
 
+const baseUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
+const prefix = process.env.EXPO_PUBLIC_BACKEND_API_PREFIX;
+
 const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_BACKEND_URL,
+  baseURL: `${baseUrl}/${prefix}`,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -15,6 +18,7 @@ api.interceptors.request.use(async (config) => {
     config.headers = config.headers || {};
     config.headers['Authorization'] = `Bearer ${token}`;
   }
+
   return config;
 });
 
